4094538f6f2e82456b60604ed153cef99d1f33cb
Exxon <exxonliu001@yahoo.com>
2025-12-01 11:49:24 +0000

    20251201: Fix: Lab/1128 && +: Lab/1128: 1

--- Lab/1128/src/main/java/Entry.java
+++ Lab/1128/src/main/java/Entry.java
@@ -1,6 +1,66 @@
 import java.util.Scanner;
+import MapReduce.FileMergeDedup;
 
 public class Entry{
+    private static final String interactiveModeMenu="0. Exit\n1. File merge with deduplication\n";
+
+    private static void handleMD(String[] args) throws Exception{
+        FileMergeDedup.main(new String[]{args[0],args[1],args[2]});
+    }
+    private static void handleArgs(String[] args) throws Exception{
+        if(args.length>0){
+            switch(args[0]){
+                case "MD":
+                    if(args.length!=4)
+                        throw new IllegalArgumentException("handleArgs(): Usage: MD <inputFile1> <inputFile2> <outputFile>");
+                    handleMD(new String[]{args[1],args[2],args[3]});
+                    break;
+                default:
+                    throw new IllegalArgumentException("handleArgs(): Unknown command: "+args[0]);
+            }
+            return;
+        }
+        throw new IllegalArgumentException("handleArgs(): Empty args");
+    }
+    private static void interactiveMode() throws Exception{
+        System.out.print(interactiveModeMenu);
+        Scanner scanner=new Scanner(System.in);
+        boolean running=true;
+        while(running)
+            try{
+                System.out.print("Enter choice: ");
+                int choice=scanner.nextInt();
+                switch(choice){
+                    case 0:
+                        running=false;
+                        System.out.println("Exit");
+                        break;
+                    case 1:
+                        System.out.print("Input file1 path: ");
+                        String inputFile1=scanner.next();
+                        System.out.print("Input file2 path: ");
+                        String inputFile2=scanner.next();
+                        System.out.print("Output file path: ");
+                        String outputFile=scanner.next();
+                        handleMD(new String[]{inputFile1,inputFile2,outputFile});
+                        break;
+                    default:
+                        System.err.println("Invalid choice: "+choice);
+                }
+            }catch(Exception e){
+                System.out.println("interactiveMode(): "+e.getMessage());
+                scanner.nextLine();
+            }
+        scanner.close();
+    }
     public static void main(String[] args){
+        try{
+            if(args.length>0)
+                handleArgs(args);
+            else
+                interactiveMode();
+        }catch(Exception e){
+            System.err.println("main(): "+e.getMessage());
+        }
     }
 }
---
+++ Lab/1128/src/main/java/MapReduce/FileMergeDedup.java
@@ -0,0 +1,57 @@
+package MapReduce;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reporter;
+import java.io.IOException;
+import org.apache.hadoop.mapred.Reducer;
+import java.util.Iterator;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class FileMergeDedup extends Configured implements Tool{
+    private static class LineMapper extends MapReduceBase implements Mapper<LongWritable,Text,Text,NullWritable>{
+        public void map(LongWritable key,Text value,OutputCollector<Text,NullWritable> output,Reporter reporter) throws IOException{
+            output.collect(value,NullWritable.get());
+        }
+    }
+    private static class LineReducer extends MapReduceBase implements Reducer<Text,NullWritable,Text,NullWritable>{
+        public void reduce(Text key,Iterator<NullWritable> values,OutputCollector<Text,NullWritable> output,Reporter reporter) throws IOException{
+            output.collect(key,NullWritable.get());
+        }
+    }
+    @Override
+    public int run(String[] args) throws Exception{
+        if(args.length==3){
+            JobConf conf=new JobConf(getConf(),FileMergeDedup.class);
+            conf.setJobName("File Merge and Deduplication");
+            conf.setOutputKeyClass(Text.class);
+            conf.setOutputValueClass(NullWritable.class);
+            conf.setMapperClass(LineMapper.class);
+            conf.setReducerClass(LineReducer.class);
+            conf.setCombinerClass(LineReducer.class);
+            conf.setJarByClass(FileMergeDedup.class);
+            FileInputFormat.addInputPath(conf,new Path(args[0]));
+            FileInputFormat.addInputPath(conf,new Path(args[1]));
+            FileOutputFormat.setOutputPath(conf,new Path(args[2]));
+            JobClient.runJob(conf);
+            return 0;
+        }
+        System.err.println("FileMergeDedup: run(): Invalid arguments");
+        ToolRunner.printGenericCommandUsage(System.err);
+        return -1;
+    }
+    public static void main(String[] args) throws Exception{
+        ToolRunner.run(new FileMergeDedup(),args);
+    }
+}
